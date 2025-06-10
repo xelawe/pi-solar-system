d4983495f74369cdefce050010b7feff638f6aac
Alex <xelawe@users.noreply.github.com>
2025-06-10 17:15:14 +0200

    Update pluto.py

--- pluto.py
+++ pluto.py
@@ -8,8 +8,8 @@ class Pluto:
     R = 3
     BOUNCE = 0.98
 
-    def __init__(self, draw):
-        self.draw = draw
+    def __init__(self, imgdraw):
+        self.imgdraw = imgdraw
         self.x = 240.0
         self.y = 0
         self.vel_x = -3.0
@@ -39,7 +39,7 @@ class Pluto:
     def draw(self):
         #self.display.set_pen(self.display.create_pen(156, 166, 183))
         #self.display.circle(int(self.x), int(self.y), Pluto.R)
-        self.draw.ellipse((int(self.x)-Pluto.R, int(self.y)-Pluto.R,int(self.x)+Pluto.R, int(self.y)+Pluto.R), fill=(156, 166, 183), outline=(255, 255, 0),width=1)
+        self.imgdraw.ellipse((int(self.x)-Pluto.R, int(self.y)-Pluto.R,int(self.x)+Pluto.R, int(self.y)+Pluto.R), fill=(156, 166, 183), outline=(255, 255, 0),width=1)
 
 
     def reset(self):
